5458fb1bf860e845e33e9302843b1c0c7162de58
MKassabeh <marwane_kassabeh@outlook.fr>
2021-11-03 15:44:37 +0100

    Create & Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    { path: '', component: LoginComponent },
+    { path: 'create', component: CreateComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
